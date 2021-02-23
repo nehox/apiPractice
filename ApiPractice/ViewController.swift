e73f0107eb5abc3b6e7469d804a4c4dc0f119481
Julien Ficerai <julien.ficerai>
2021-02-23 22:18:47 +0100

    Initial Commit

---
+++ ApiPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiPractice
+//
+//  Created by jficerai on 23/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
